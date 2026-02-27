af3e3f5bfdbf8db59b015c479bde10ef76b3fe4b
Fhelipe Abel <abelfhp@gmail.com>
2026-02-27 16:07:28 -0300

    Atualização

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,19 +21,14 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/favicon-light.ico',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/favicon-dark.ico',
         media: '(prefers-color-scheme: dark)',
       },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
     ],
-    apple: '/apple-icon.png',
   },
 }
 
