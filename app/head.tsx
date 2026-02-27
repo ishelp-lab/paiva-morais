470f4873898fc6b922dc9b7e5dabab512e772497
Fhelipe Abel <abelfhp@gmail.com>
2026-02-27 14:06:38 -0300

    Atualização

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="apple-touch-icon" href="/apple-icon.png" />
+      <meta name="theme-color" content="#08BBB7" />
+    </>
+  )
+}
