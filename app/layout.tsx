15697ec17aa0c7b6477ef14449fa7282d94076af
ishelp-lab <ishelp.adm@gmail.com>
2026-05-28 13:47:25 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,13 @@ export default function RootLayout({
           <CookieBanner />
         </ThemeProvider>
         <Analytics />
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wy9dka9c73");
+</script>
       </body>
     </html>
   )
