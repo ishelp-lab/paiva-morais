438890bfb26745622f1a7c314ca4cff561f7b702
ishelp-lab <ishelp.adm@gmail.com>
2026-05-28 13:41:08 -0300

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,13 @@ export default function Head() {
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <link rel="apple-touch-icon" href="/apple-icon.png" />
       <meta name="theme-color" content="#08BBB7" />
+      <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wy9dka9c73");
+</script>
     </>
   )
 }
